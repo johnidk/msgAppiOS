afb9be72e6e7d02d3532e2f7e5377ea857e5c35b
johnidk <johngabrielbarreto1337@gmail.com>
2024-04-08 20:41:28 -0300

    Initial Commit

---
+++ msgAppFirebaseiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  msgAppFirebaseiOS
+//
+//  Created by João Gabriel Lavareda Ayres Barreto on 08/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
